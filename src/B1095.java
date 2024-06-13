c8864c477646afd2151dd89a7db71d1b149b0642
Cleber <cleber.estevam@hotmail.com>
2024-06-13 17:43:49 -0300

    Explicação

--- src/B1095.java
+++ src/B1095.java
@@ -8,9 +8,9 @@ public class B1095{
 
         for (int i = 1, j = 60; i < 85 && j >= 0;i +=3, j-=5){
 
-       // for (int i = 1; i < 50; i += 3) {
-
-          //  for (int j = 60; j > 0; j -= 5) {
+       /*
+        * Desta forma você consegue fazer dois for que serão executados juntos
+        */
 
                 System.out.println("I=" + i + " J=" + j); }
 
